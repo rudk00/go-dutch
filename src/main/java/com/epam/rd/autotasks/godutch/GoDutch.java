0d8e46d24eefc7252eb5df72cd94d5480b60694a
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-01 17:46:25 +0300

    Payment calculation

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,20 @@
 package com.epam.rd.autotasks.godutch;
+import java.util.Scanner;
 
 public class GoDutch {
 
     public static void main(String[] args) {
-        //Write code here
+        Scanner scanner = new Scanner(System.in);
+        int bill = scanner.nextInt();
+        int numberOfFriends = scanner.nextInt();
+        int billWithTips = (bill / 10) + bill;
+        if (bill < 0) {
+            System.out.println("Bill total amount cannot be negative");
+        } else if (numberOfFriends <= 0) {
+            System.out.println("Number of friends cannot be negative or zero");
+        } else {
+            int payment = billWithTips / numberOfFriends;
+            System.out.println(payment);
+        }
     }
 }
